9c8b94d72a6dc049990929473b2cf7e89f5e8cce
minimouse0 <473147334@qq.com>
2024-01-23 00:35:57 +0800

    我也不知道这些修改的意义是什么

--- ts/index.ts
+++ ts/index.ts
@@ -3,7 +3,7 @@
 
 // LNSDK
 /// <reference path="../lnsdkdts/index.d.ts"/>
-import { ScriptDone } from './src/lib/Events/Process/Init';
+import { ScriptDone } from './src/lib/Events/Process';
 
 import './src/index';
 
--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,18 +1,24 @@
-import "./${CONF.src_dir}/${CONF.main}";export const LNCONF=JSON.parse("${JSON.stringify(CONF)}");
-/*import {Platform, SupportedPlatforms} from "./lib/Platform";
-import { LNLogger } from "./lib/LNSDKT"
-import {InitEvent} from "./lib/Events/Process/Init";
+/// <reference path="./lib/Types/src/GameAPI/Player.d.ts"/>
+export const LNCONF=JSON.parse("${JSON.stringify(CONF)}");
+import "src/index.ts";
+/*
+import {Platform, SupportedPlatforms} from "./lib/Platform";
+import { Logger } from "./lib/LNSDKT"
+import {InitEvent} from "./lib/Events/Process";
 import {Command, CommandResult} from "./lib/Game/Command";
+import {Player} from "./lib/Game/Player"
+
 class newcmd extends Command{
     constructor(){
         super("test")
     }
     callback(result:CommandResult) {
-        LNLogger.info("abcdefg")
+        Logger.info("abcdefg")
     }
 }
 
 InitEvent.on((e)=>{
-    LNLogger.info(1,2,3,4,5,6)
+    Logger.info(1,2,3,4,5,6)
     Command.register(new newcmd())
-});*/
+});
+*/
