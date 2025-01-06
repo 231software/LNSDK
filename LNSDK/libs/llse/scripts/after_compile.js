ff976f9a10a1477a74456ac3f32c3879820c5385
Minimouse <473147334@qq.com>
2025-01-06 18:32:08 +0800

    为legacyscriptengine自动生成manifest.json

---
+++ LNSDK/libs/llse/scripts/after_compile.js
@@ -0,0 +1,19 @@
+const fs=require("fs")
+const plugin_info=JSON.parse(Buffer.from((process.argv[2]),"base64").toString("utf-8"))
+process.chdir(plugin_info.plugin_conf.build_dir+"/llse/"+plugin_info.plugin_conf.plugin_dir_name)
+const manifestFile=fs.openSync("manifest.json","w+")
+const llmanifest={
+    //levilamina要求插件名必须与插件目录相同，此处只好取插件目录名
+    name:plugin_info.plugin_conf.plugin_dir_name,
+    entry:plugin_info.plugin_conf.main+".js",
+    type:"lse-nodejs",
+    dependencies:[
+        {
+            name:"legacy-script-engine-nodejs"
+        }
+    ]
+}
+llmanifest.author=plugin_info.plugin_conf.author
+llmanifest.description=plugin_info.plugin_conf.description
+fs.writeFileSync("manifest.json",JSON.stringify(llmanifest,undefined,4))
+fs.close(manifestFile)
